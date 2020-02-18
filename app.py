296b2f2973849f4391626f86e2beeaa8f2c58644
李明叡 <leeray19901208@gmail.com>
2020-02-18 15:14:19 -0800

    Define menu()

---
+++ app.py
@@ -0,0 +1,36 @@
+"""
+- Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit:
+
+- Add movies
+- See movies
+- Find a movie
+- Stop running the program
+
+Tasks:
+[]: Where to store movies
+[]: What is the format of a movie
+[]: Show the user the main interface and get their input
+[]: Allow users to add movies
+[]: Show all their movies
+[]: Find a movie
+[]: Stop running the program when they type 'q'
+"""
+
+
+def menu():
+    user_input = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit: ")
+
+    while user_input != 'q':
+        if user_input == 'a':
+            add_movie()
+        elif user_input == 'l':
+            show_movies()
+        elif user_input == 'f':
+            find_movie()
+        else:
+            print("Unknown command-please try again.")
+
+        user_input = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit: ")
+
+
+menu()
