84280ff8ae96d9786bb01c5274697e9b1c31eeb1
李明叡 <leeray19901208@gmail.com>
2020-02-18 16:21:25 -0800

    Add find_movie function

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def menu():
         if user_input == 'a':
             add_movie()
         elif user_input == 'l':
-            show_movies()
+            show_movies(movies)
         elif user_input == 'f':
             find_movie()
         else:
@@ -38,7 +38,7 @@ def menu():
 def add_movie():
     name = input('Enter the movie name: ')
     director = input('Enter the movie director: ')
-    year = int(input('Enter the movie release year: '))
+    year = input('Enter the movie release year: ')
 
     movies.append({
         'name': name,
@@ -47,8 +47,8 @@ def add_movie():
     })
 
 
-def show_movies():
-    for movie in movies:
+def show_movies(movies_list):
+    for movie in movies_list:
         show_movie_details(movie)
 
 
@@ -56,6 +56,25 @@ def show_movie_details(movie):
     print(f"Name: {movie['name']}")
     print(f"Director: {movie['director']}")
     print(f"Release year: {movie['year']}")
+
+
+def find_movie():
+    find_by = input("What property of the movie are you looking for? ")
+    looking_for = input("What are you searching for? ")
+
+    found_movies = find_by_attribute(movies, looking_for, lambda x: x[find_by])
+
+    show_movies(found_movies)
+
+
+def find_by_attribute(items, expected, finder):
+    found = []
+
+    for i in items:
+        if finder(i) == expected:
+            found.append(i)
+
+    return found
 
 
 menu()
